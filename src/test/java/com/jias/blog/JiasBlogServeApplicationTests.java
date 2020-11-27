cb57498a772e3c4de4f34741f10c07cd0d52cf44
jias <Jias96@163.com>
2020-11-27 14:14:41 +0800

    创建项目

---
+++ src/test/java/com/jias/blog/JiasBlogServeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jias.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiasBlogServeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
